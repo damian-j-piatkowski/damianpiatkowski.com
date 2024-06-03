1f91d01adca0e9ca9ecbcdb02a851541e23f50cc
Damian Piatkowski <damian.j.piatkowski@gmail.com>
2024-06-03 10:24:29 +0200

    Simplified media queries coverage introduced.

--- utilities/resize_images.bat
+++ utilities/resize_images.bat
@@ -12,8 +12,8 @@ set "input_dir=%~1"
 set "output_dir=%~1"
 
 :: Define output resolutions for 4:5 and 16:9 aspect ratios
-set resolutions_4x5=320x400 480x600 640x800 750x938 828x1035 960x1200 1200x1500
-set resolutions_16x9=1024x576 1280x720 1920x1080 2560x1440
+set resolutions_4x5=320x400 640x800 960x1200 1200x1500
+set resolutions_16x9=1920x1080 2560x1440
 
 :: Loop through 4:5 resolutions and create resized images
 for %%R in (%resolutions_4x5%) do (
