01d0d26bf7974782ab3be604fb603ebe95064da2
Damian Piatkowski <damian.j.piatkowski@gmail.com>
2024-08-07 13:57:01 +0200

    New volumes added to docker-compose files.

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
+import os
+
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
+    if os.getenv("FLASK_ENV") == "development":
+        import debugpy
+        debugpy.listen(('0.0.0.0', 5678))
+        print("Debugger is active and waiting for connection...")
+        debugpy.wait_for_client()
+
     app.run()
